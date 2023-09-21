12bcebff52c62071742555277b4a2c818c419edd
JohnChristianIdul <idulchristian@gmail.com>
2023-09-21 19:45:38 +0800

    Initial commit

---
+++ PracticalExamPrelim/PracticalExamPrelim_IDUL_JOHNCHRISTIAN/MidtermExam/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Procedure)
+admin.site.register(MedicalHistory)
+admin.site.register(DocToProcedure)
---
+++ PracticalExamPrelim/PracticalExamPrelim_IDUL_JOHNCHRISTIAN/MidtermExam/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+# Create your models here.
+class Person(models.Model):
+    PID = models.AutoField(primary_key=True)
+    Fname = models.CharField(max_length=50, null=False)
+    Lname = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.Fname
+
+class Doctor(Person):
+    Specialty = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.Fname
+
+class Patient(Person):
+    attending_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Fname
+
+class Procedure(models.Model):
+    Procedure_Core = models.IntegerField(primary_key=True)
+    Procedure_Name = models.CharField(max_length=50, null=False)
+    Cost = models.IntegerField()
+
+    def __str__(self):
+        return str(self.Procedure_Name)
+
+class MedicalHistory(models.Model):
+    proceed = models.ManyToManyField(Procedure)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+
+class DocToProcedure(models.Model):
+    doc = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    procedure = models.ForeignKey(Procedure, related_name="procedure_done", on_delete=models.CASCADE)
+    TimeOfProcedure = models.TimeField()
+    DateOfProcedure = models.DateField()
+
+    def __str__(self):
+        return str(self.DateOfProcedure) + " " + str(self.TimeOfProcedure)
+
